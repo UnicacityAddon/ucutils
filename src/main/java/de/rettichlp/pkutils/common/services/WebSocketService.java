4081837fe434aa3008ad0d036fe8c1d33c77c1c5
RettichLP <rettichlp01@gmail.com>
2025-11-05 22:03:21 +0100

    feat: enhance WebSocket message type handling with improved parsing and logging

--- src/main/java/de/rettichlp/pkutils/common/services/WebSocketService.java
+++ src/main/java/de/rettichlp/pkutils/common/services/WebSocketService.java
@@ -1,5 +1,7 @@
 package de.rettichlp.pkutils.common.services;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.http.WebSocket;
@@ -7,6 +9,8 @@ import java.util.concurrent.CompletionStage;
 
 import static de.rettichlp.pkutils.PKUtils.LOGGER;
 import static de.rettichlp.pkutils.PKUtils.syncService;
+import static de.rettichlp.pkutils.common.services.WebSocketService.WebSocketMessageType.fromValue;
+import static java.util.Arrays.stream;
 
 public class WebSocketService implements WebSocket.Listener {
 
@@ -20,20 +24,35 @@ public class WebSocketService implements WebSocket.Listener {
     public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
         LOGGER.info("WebSocket data received: {}", data);
 
-        WebSocketMessageType webSocketMessageType = WebSocketMessageType.valueOf((String) data);
+        WebSocketMessageType webSocketMessageType = fromValue(data);
         switch (webSocketMessageType) {
             case REQUEST_UPDATE_FACTION_MEMBERS -> syncService.syncFactionMembersWithApi();
             case REQUEST_UPDATE_BLACKLIST_REASONS -> syncService.syncBlacklistReasonsFromApi();
             case REQUEST_CHECK_FOR_UPDATES -> syncService.checkForUpdates();
+            case UNKNOWN -> LOGGER.warn("Received unknown WebSocket message type: {}", data);
         }
 
         return WebSocket.Listener.super.onText(webSocket, data, last);
     }
 
-    private enum WebSocketMessageType {
+    @Getter
+    @AllArgsConstructor
+    enum WebSocketMessageType {
 
-        REQUEST_UPDATE_FACTION_MEMBERS,
-        REQUEST_UPDATE_BLACKLIST_REASONS,
-        REQUEST_CHECK_FOR_UPDATES
+        UNKNOWN("UNKNOWN"),
+        REQUEST_UPDATE_FACTION_MEMBERS("REQUEST_UPDATE_FACTION_MEMBERS"),
+        REQUEST_UPDATE_BLACKLIST_REASONS("REQUEST_UPDATE_BLACKLIST_REASONS"),
+        REQUEST_CHECK_FOR_UPDATES("REQUEST_CHECK_FOR_UPDATES");
+
+        private final String value;
+
+        public static WebSocketMessageType fromValue(CharSequence value) {
+            String typeString = ((String) value).split(":")[0];
+
+            return stream(values())
+                    .filter(webSocketMessageType -> webSocketMessageType.getValue().contentEquals(typeString))
+                    .findFirst()
+                    .orElse(UNKNOWN);
+        }
     }
 }
