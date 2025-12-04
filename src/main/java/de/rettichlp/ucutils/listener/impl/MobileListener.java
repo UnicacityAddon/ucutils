1c09701aa4ee691ef30cae1b275b71618f4e1316
RettichLP <rettichlp01@gmail.com>
2025-12-04 01:36:14 +0100

    fix: update mobile number pattern for improved message matching

--- src/main/java/de/rettichlp/ucutils/listener/impl/MobileListener.java
+++ src/main/java/de/rettichlp/ucutils/listener/impl/MobileListener.java
@@ -14,7 +14,7 @@ import static java.util.regex.Pattern.compile;
 @UCUtilsListener
 public class MobileListener implements IMessageReceiveListener {
 
-    private static final Pattern MOBILE_NUMBER_PATTERN = compile("^(?:\\[UC])?(?<playerName>[a-zA-Z0-9_]+) gehört die Nummer (?<number>\\d+)\\.$");
+    private static final Pattern MOBILE_NUMBER_PATTERN = compile("^Nummer von (?:\\[UC])?(?<playerName>[a-zA-Z0-9_]+): (?<number>\\d+)$");
     private static final Pattern MOBILE_SMS_RECEIVE_PATTERN = compile("^Dein Handy klingelt! Eine Nachricht von (?:\\[UC])?(?<playerName>[a-zA-Z0-9_]+) \\((?<number>\\d+)\\)\\.$");
 
     @Override
