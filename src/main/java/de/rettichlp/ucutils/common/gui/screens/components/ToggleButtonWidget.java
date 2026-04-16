b509849512ee837328ce920fb7a1fd3956f560bd
RettichLP <rettichlp01@gmail.com>
2026-04-16 11:30:33 +0200

    bump: update dependencies (Minecraft, yarn mappings, fabric API) and adjust code to Minecraft 1.21.11
    Co-authored-by: AI

--- src/main/java/de/rettichlp/ucutils/common/gui/screens/components/ToggleButtonWidget.java
+++ src/main/java/de/rettichlp/ucutils/common/gui/screens/components/ToggleButtonWidget.java
@@ -1,8 +1,8 @@
 package de.rettichlp.ucutils.common.gui.screens.components;
 
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.input.AbstractInput;
-import net.minecraft.text.Text;
 
 import java.util.function.Consumer;
 
@@ -16,11 +16,11 @@ import static net.minecraft.util.Formatting.RED;
 
 public class ToggleButtonWidget extends ButtonWidget {
 
-    private final Text text;
+    private final net.minecraft.text.Text text;
     private final Consumer<Boolean> changeListener;
     private boolean state;
 
-    public ToggleButtonWidget(Text text, Consumer<Boolean> changeListener, boolean defaultState) {
+    public ToggleButtonWidget(net.minecraft.text.Text text, Consumer<Boolean> changeListener, boolean defaultState) {
         super(0, 0, 150, 20, empty(), button -> {}, DEFAULT_NARRATION_SUPPLIER);
         this.text = text;
         this.changeListener = changeListener;
@@ -36,11 +36,15 @@ public class ToggleButtonWidget extends ButtonWidget {
         setMessage(getText());
     }
 
+    @Override
+    protected void drawIcon(DrawContext context, int mouseX, int mouseY, float deltaTicks) {
+    }
+
     public void updateText() {
         setMessage(getText());
     }
 
-    private Text getText() {
+    private net.minecraft.text.Text getText() {
         return this.text.copy()
                 .append(of(":").copy().formatted(GRAY)).append(" ")
                 .append(this.state ? ON.copy().formatted(GREEN) : OFF.copy().formatted(RED));
