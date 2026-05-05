3157d64a5200d42b510cca2ad0bce6571e0b8e3e
RettichLP <rettichlp01@gmail.com>
2026-05-05 18:23:11 +0200

    Add dealer tracking system with navigation commands and visited status handling

---
+++ src/main/java/de/rettichlp/ucutils/command/faction/DealerCommand.java
@@ -0,0 +1,34 @@
+package de.rettichlp.ucutils.command.faction;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import de.rettichlp.ucutils.common.registry.CommandBase;
+import de.rettichlp.ucutils.common.registry.UCUtilsCommand;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import org.jetbrains.annotations.NotNull;
+
+import static de.rettichlp.ucutils.UCUtils.messageService;
+import static de.rettichlp.ucutils.UCUtils.player;
+import static de.rettichlp.ucutils.UCUtils.storage;
+import static java.util.Comparator.comparingDouble;
+import static net.minecraft.text.Text.empty;
+
+@UCUtilsCommand(label = "dealer")
+public class DealerCommand extends CommandBase {
+
+    @Override
+    public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
+        return node
+                .executes(context -> {
+                    player.sendMessage(empty(), false);
+
+                    messageService.sendModMessage("Dealer Orte:", false);
+                    storage.getDealers().stream()
+                            .sorted(comparingDouble(value -> value.getType().getBlockPos().getSquaredDistance(player.getBlockPos())))
+                            .forEach(dealer -> messageService.sendModMessage(dealer.getText(), false));
+
+                    player.sendMessage(empty(), false);
+
+                    return 1;
+                });
+    }
+}
