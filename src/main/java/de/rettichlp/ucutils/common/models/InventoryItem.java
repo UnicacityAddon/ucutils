f0804e0e716b1e77c361afa9a9e2fd7ba140beaf
RettichLP <rettichlp01@gmail.com>
2026-05-20 11:53:06 +0200

    Remove Ingredient model and migrate functionality to InventoryItem

--- src/main/java/de/rettichlp/ucutils/common/models/InventoryItem.java
+++ src/main/java/de/rettichlp/ucutils/common/models/InventoryItem.java
@@ -2,6 +2,11 @@ package de.rettichlp.ucutils.common.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+
+import java.util.Optional;
+
+import static java.util.Arrays.stream;
 
 @Getter
 @AllArgsConstructor
@@ -9,20 +14,33 @@ public enum InventoryItem {
 
     // drugs
     POWDER("Pulver", true),
+    MEDICINAL_HERBS("Medizinische Kräuter"),
     HERBS("Kräuter", true),
     CRYSTALS("Kristalle", true),
     GRAB_BAG("Wundertüte", true),
 
     // medical
-    COUGH_SYRUP("Hustensaft", false),
-    PAINKILLERS("Schmerzmittel", false),
-    ANTIBIOTICS("Antibiotika", false),
+    COUGH_SYRUP("Hustensaft"),
+    PAINKILLERS("Schmerzmittel"),
+    ANTIBIOTICS("Antibiotika"),
 
     // other
-    MASK("Maske", false),
-    GUN_POWDER("Schwarzpulver", false),
-    IRON("Eisen", false);
+    MASK("Maske"),
+    IRON("Eisen"),
+    GUN_POWDER("Schwarzpulver"),
+    KEVLAR_FIBERS("Kevlarfasern");
 
     private final String displayName;
     private final boolean drugBankItem;
+
+    InventoryItem(String displayName) {
+        this.displayName = displayName;
+        this.drugBankItem = false;
+    }
+
+    public static @NonNull Optional<InventoryItem> fromDisplayName(String displayName) {
+        return stream(values())
+                .filter(inventoryItem -> inventoryItem.getDisplayName().equals(displayName))
+                .findFirst();
+    }
 }
