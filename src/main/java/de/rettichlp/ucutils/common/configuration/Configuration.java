d9062a797ac6113c317d4f23a385919fbf32368a
RettichLP <rettichlp01@gmail.com>
2026-05-24 20:00:31 +0200

    Refactor PayDay notification logic: remove redundant checks in `Configuration` and centralize handling in `EconomyListener`
    Co-authored-by: AI

--- src/main/java/de/rettichlp/ucutils/common/configuration/Configuration.java
+++ src/main/java/de/rettichlp/ucutils/common/configuration/Configuration.java
@@ -14,9 +14,6 @@ import java.util.Map;
 
 import static de.rettichlp.ucutils.UCUtils.LOGGER;
 import static de.rettichlp.ucutils.UCUtils.api;
-import static de.rettichlp.ucutils.UCUtils.configuration;
-import static de.rettichlp.ucutils.UCUtils.messageService;
-import static de.rettichlp.ucutils.UCUtils.notificationService;
 import static java.nio.file.Files.newBufferedReader;
 import static java.nio.file.Files.newBufferedWriter;
 
@@ -38,19 +35,6 @@ public class Configuration {
 
         if (this.minutesSinceLastPayDay % 10 == 0) {
             new Thread(this::saveToFile).start(); // asynchronously save every active 10 minutes
-        }
-
-        if (configuration.getMoneyBankAmount() > 100000) {
-            switch (this.minutesSinceLastPayDay) {
-                case 50 -> {
-                    messageService.sendModMessage("Du hast in 10 Minuten PayDay und über 100000$ auf der Bank!", false);
-                    notificationService.notificationSound(1);
-                }
-                case 55 -> {
-                    messageService.sendModMessage("Du hast in 5 Minuten PayDay und über 100000$ auf der Bank!", false);
-                    notificationService.notificationSound(2);
-                }
-            }
         }
     }
 
