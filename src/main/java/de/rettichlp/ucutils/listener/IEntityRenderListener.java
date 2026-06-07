90e7ffc9fdc3d1d43a83489e0b0f2d682b72f6fe
RettichLP <rettichlp01@gmail.com>
2026-06-07 22:00:11 +0200

    Remove `IEntityRenderListener` and related entity render highlighting functionality

--- src/main/java/de/rettichlp/ucutils/listener/IEntityRenderListener.java
+++
@@ -1,8 +0,0 @@
-package de.rettichlp.ucutils.listener;
-
-import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderContext;
-
-public interface IEntityRenderListener extends IUCUtilsListener {
-
-    void onEntityRender(WorldRenderContext context);
-}
--- src/main/java/de/rettichlp/ucutils/listener/impl/CarListener.java
+++ src/main/java/de/rettichlp/ucutils/listener/impl/CarListener.java
@@ -1,47 +1,24 @@
 package de.rettichlp.ucutils.listener.impl;
 
 import de.rettichlp.ucutils.common.registry.UCUtilsListener;
-import de.rettichlp.ucutils.listener.IEntityRenderListener;
 import de.rettichlp.ucutils.listener.IMessageReceiveListener;
-import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderContext;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.text.Text;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static de.rettichlp.ucutils.UCUtils.commandService;
-import static de.rettichlp.ucutils.UCUtils.configuration;
-import static de.rettichlp.ucutils.UCUtils.renderService;
 import static de.rettichlp.ucutils.UCUtils.storage;
 import static java.lang.Integer.parseInt;
-import static java.util.Optional.ofNullable;
 import static java.util.regex.Pattern.compile;
-import static net.minecraft.util.Formatting.AQUA;
 
 @UCUtilsListener
-public class CarListener implements IEntityRenderListener, IMessageReceiveListener {
+public class CarListener implements IMessageReceiveListener {
 
     private static final Pattern CAR_UNLOCK_PATTERN = compile("^\\[Car] Du hast deinen .+ aufgeschlossen\\.$");
     private static final Pattern CAR_LOCK_PATTERN = compile("^\\[Car] Du hast deinen .+ abgeschlossen\\.$");
     private static final Pattern CAR_LOCKED_OWN_PATTERN = compile("^\\[Car] Dein Fahrzeug ist abgeschlossen\\.$");
     private static final Pattern CAR_FIND_PATTERN = compile("^\\[Car] Das Fahrzeug befindet sich bei » X: (?<x>-?\\d+) \\| Y: (?<y>-?\\d+) \\| Z: (?<z>-?\\d+)$");
-
-    @Override
-    public void onEntityRender(WorldRenderContext context) {
-        MatrixStack matrices = context.matrices();
-        VertexConsumerProvider vertexConsumers = context.consumers();
-        ClientWorld world = MinecraftClient.getInstance().world;
-
-        if (world != null && configuration.getOptions().car().highlight() && !storage.isPremium()) {
-            ofNullable(storage.getMinecartEntityToHighlight())
-                    .map(minecartEntity -> world.getEntityById(minecartEntity.getId()))
-                    .ifPresent(minecartEntity -> renderService.renderTextAboveEntity(matrices, vertexConsumers, minecartEntity, Text.of("🚗").copy().formatted(AQUA), 0.05F));
-        }
-    }
 
     @Override
     public boolean onMessageReceive(Text text, String message) {
