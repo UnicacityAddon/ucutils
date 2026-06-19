588fc4f3c9293bd6f4607bd49c7823d0128f06ff
RettichLP <rettichlp01@gmail.com>
2026-06-19 09:39:28 +0200

    Remove `RenderListener` and `IHudRenderListener`, integrate notification and widget rendering directly into `GuiMixin`

--- src/main/java/de/rettichlp/ucutils/listener/IHudRenderListener.java
+++
@@ -1,9 +0,0 @@
-package de.rettichlp.ucutils.listener;
-
-import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.GuiGraphicsExtractor;
-
-public interface IHudRenderListener extends IUCUtilsListener {
-
-    void onHudRender(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker);
-}
