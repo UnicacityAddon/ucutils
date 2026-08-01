a02685a6ad653d9c5465d25b686271899ec5247a
RettichLP <rettichlp01@gmail.com>
2026-08-01 18:19:09 +0200

    Refactor CorpseCountWidget to improve performance by caching faction corpse counts and update widget visibility logic

--- src/main/java/de/rettichlp/ucutils/common/gui/widgets/CorpseCountWidget.java
+++ src/main/java/de/rettichlp/ucutils/common/gui/widgets/CorpseCountWidget.java
@@ -8,7 +8,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -20,7 +19,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 
 import static de.rettichlp.ucutils.UCUtils.player;
-import static de.rettichlp.ucutils.UCUtils.storage;
 import static de.rettichlp.ucutils.common.services.RenderService.keyValue;
 import static net.minecraft.network.chat.CommonComponents.SPACE;
 import static net.minecraft.network.chat.Component.empty;
@@ -32,19 +30,20 @@ import static net.minecraft.world.item.Items.WITHER_SKELETON_SKULL;
 
 public class CorpseCountWidget extends AbstractTRCTextWidget<CorpseCountWidget.Configuration> {
 
+    private LinkedHashMap<Faction, Long> factionCorpseCountMap = new LinkedHashMap<>();
+
     @Override
     public Component text() {
-        LinkedHashMap<Faction, Long> factionCorpseCountMap = getFactionCorpseCountMap();
+        this.factionCorpseCountMap = getFactionCorpseCountMap();
 
-        MutableComponent component = empty()
-                .append(translatable("ucutils.options.widgets.corpse_count.label"));
+        MutableComponent component = empty();
 
-        factionCorpseCountMap.forEach((faction, amount) -> component
+        this.factionCorpseCountMap.forEach((faction, amount) -> component
                 .append(literal(amount.toString()).withColor(faction.getColor()))
                 .append(literal("x").withColor(GRAY))
                 .append(SPACE));
 
-        return keyValue("Corpses", component);
+        return keyValue(translatable("ucutils.options.widgets.corpse_count.label"), component);
     }
 
     @Override
@@ -68,7 +67,7 @@ public class CorpseCountWidget extends AbstractTRCTextWidget<CorpseCountWidget.C
     @Override
     public boolean isVisible() {
         // visible if in the position options screen to allow positioning
-        return super.isVisible() && (storage.getActiveServices() > 0 || isWidgetPositionScreen());
+        return super.isVisible() && (!this.factionCorpseCountMap.isEmpty() || isWidgetPositionScreen());
     }
 
     private @NonNull LinkedHashMap<Faction, Long> getFactionCorpseCountMap() {
@@ -76,9 +75,13 @@ public class CorpseCountWidget extends AbstractTRCTextWidget<CorpseCountWidget.C
         List<Component> corpseNames = getCorpseItemEntities().stream().map(Entity::getCustomName).toList();
 
         for (Faction faction : Faction.values()) {
-            factionCorpseCountMap.put(faction, corpseNames.stream()
+            long count = corpseNames.stream()
                     .filter(name -> name.getString().contains(faction.getIcon()))
-                    .count());
+                    .count();
+
+            if (count > 0) {
+                factionCorpseCountMap.put(faction, count);
+            }
         }
 
         return factionCorpseCountMap;
